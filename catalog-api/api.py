d44ef421588a311df7f5249fd962701b917f238b
Weder Ribas <wederribas@gmail.com>
2018-03-05 00:54:54 -0300

    Add new routes to manage items and users / Add all listing in JSON

--- catalog-api/api.py
+++ catalog-api/api.py
@@ -29,12 +29,21 @@ db.init_app(app)
 db.create_all(app=app)
 
 
-@app.route('/')
-def hello_world():
-    return jsonify({
-        'status': 'success',
-        'message': 'What is up doc!'
-    })
+@app.route('/catalog.json')
+def list_catalog():
+    categories = Category.query.order_by(Category.name).all()
+    categories_dict = [c.serialize for c in categories]
+    for category in range(len(categories_dict)):
+        items = (
+            Item.query
+            .filter_by(category_id=categories_dict[category]['id'])
+            .order_by(Item.name)
+            .all()
+        )
+        items_dict = [i.serialize for i in items]
+        if items_dict:
+            categories_dict[category]['Items'] = items_dict
+    return jsonify(Category=categories_dict)
 
 
 @app.route('/categories', methods=['GET'])
@@ -45,13 +54,13 @@ def list_all_categories():
 
 @app.route('/categories', methods=['POST'])
 def add_category():
-    new_category = Category(name=request.form['name'])
+    new_category = Category(name=request.form['name'].strip())
     db.session.add(new_category)
     db.session.commit()
 
     return jsonify({
         'status': 'success',
-        'message': 'Category has been successfully created'
+        'message': 'Category has been successfully added'
     })
 
 
@@ -65,14 +74,16 @@ def edit_category(category_id):
             'message': 'Category not found'
         })
         response.status_code = 204
+
         return response
 
-    category.name = request.form['new_name']
+    if request.form.get('name'):
+        category.name = request.form['name'].strip()
     db.session.commit()
 
     return jsonify({
         'status': 'success',
-        'message': 'Category name has been updated successfully'
+        'message': 'Category has been successfully updated'
     })
 
 
@@ -86,6 +97,7 @@ def delete_category(category_id):
             'message': 'Category not found'
         })
         response.status_code = 204
+
         return response
 
     db.session.delete(category)
@@ -94,6 +106,96 @@ def delete_category(category_id):
     return jsonify({
         'status': 'success',
         'message': 'Category has been successfully deleted'
+    })
+
+
+@app.route('/items', methods=['GET'])
+def list_all_items():
+    items= Item.query.all()
+    return jsonify(Items=[i.serialize for i in items])
+
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    new_item = Item(
+        name=request.form['name'].strip(),
+        description=request.form['description'].strip(),
+        image_url=request.form['image_url'].strip(),
+        category_id=request.form['category_id'].strip()
+    )
+    db.session.add(new_item)
+    db.session.commit()
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Item has been successfully added'
+    })
+
+
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def edit_item(item_id):
+    item = Item.query.filter_by(id=item_id).first()
+
+    if item is None:
+        response = jsonify({
+            'status': 'error',
+            'message': 'Item not found'
+        })
+        response.status_code = 204
+
+        return response
+
+    if request.form.get('name'):
+        item.name = request.form['name'].strip()
+    if request.form.get('description'):
+        item.description = request.form['description'].strip()
+    if request.form.get('image_url'):
+        item.image_url = request.form['image_url'].strip()
+    if request.form.get('category_id'):
+        item.category_id = request.form['category_id'].strip()
+    db.session.commit()
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Item has benn successfully updated'
+    })
+
+
+@app.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    item = Item.query.filter_by(id=item_id).first()
+
+    if item is None:
+        response = jsonify({
+            'status': 'error',
+            'message': 'Item not found'
+        })
+        response.status_code = 204
+
+        return response
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify({
+        'status': 'success',
+        'message': 'Item has been successfully deleted'
+    })
+
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    new_user = User(
+        uid=request.form['uid'].strip(),
+        name=request.form['name'].strip(),
+        avatar=request.form['avatar'].strip()
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        'status': 'success',
+        'message': 'User has been successfully added'
     })
 
 
