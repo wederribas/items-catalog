fabcc6092361deaf708a2b3789395293d04cae92
Weder Ribas <wederribas@gmail.com>
2018-03-30 23:02:31 -0300

    Adjust docker conf related files to new folders names

--- docker/shell-flaskapi.sh
+++ docker/shell-flaskapi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it --user root catalog_api bash -c "cd /usr/src/catalog-api; exec /bin/bash";
+docker exec -it --user root catalog_api bash -c "cd /usr/src/catalog_api; exec /bin/bash";
