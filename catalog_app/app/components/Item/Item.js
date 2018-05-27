0a767c00963de8e0e0526fb36c0ff165d556dff3
Weder Ribas <wederribas@gmail.com>
2018-05-27 11:53:43 -0300

    Add action buttons to Item description page for logged users

--- catalog_app/app/components/Item/Item.js
+++ catalog_app/app/components/Item/Item.js
@@ -1,21 +1,35 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 import {Title} from 'components'
 import {fetchItem} from 'helpers/api'
-import {description} from './styles.css'
-import {listContainer} from '../../assets/styles/styles.css'
+import {description, actionButtons} from './styles.css'
+import {listContainer, formLink} from '../../assets/styles/styles.css'
+import {AuthedUserContext} from 'context/authedUserContext'
 
 class Item extends Component {
   state = {
     item: null,
+    isUserActionAllowed: false,
   }
+
+  static propTypes = {
+    isAuthed: PropTypes.bool.isRequired,
+  }
+
   componentDidMount() {
     fetchItem(this.props.match.params.id).then(resp => {
       this.setState({
         item: resp,
+        isUserActionAllowed: resp.is_user_owner,
       })
     })
   }
+
   render() {
+    const displayActionButtons =
+      this.state.isUserActionAllowed && this.props.isAuthed
+
     return (
       <div className={listContainer}>
         {this.state.item ? (
@@ -25,6 +39,25 @@ class Item extends Component {
               <b>{'Description: '}</b>
               {this.state.item.description}
             </p>
+            {displayActionButtons ? (
+              <div className={actionButtons}>
+                <Link
+                  className={formLink}
+                  style={{margin: 0}}
+                  to={'/edit/' + this.state.item.id}
+                >
+                  Edit
+                </Link>
+                <span> | </span>
+                <Link
+                  className={formLink}
+                  style={{margin: 0}}
+                  to={'/delete/' + this.state.item.id}
+                >
+                  Delete
+                </Link>
+              </div>
+            ) : null}
           </section>
         ) : (
           <span>{'Loading'}</span>
@@ -34,4 +67,8 @@ class Item extends Component {
   }
 }
 
-export default Item
+export default props => (
+  <AuthedUserContext.Consumer>
+    {isAuthed => <Item {...props} isAuthed={isAuthed} />}
+  </AuthedUserContext.Consumer>
+)
