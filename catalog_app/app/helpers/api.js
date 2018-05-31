b47c67aefaad395024754649d70dee5fe756ba7d
Weder Ribas <wederribas@gmail.com>
2018-05-31 19:15:08 -0300

    Simplify fetch object by using default params object

--- catalog_app/app/helpers/api.js
+++ catalog_app/app/helpers/api.js
@@ -1,12 +1,16 @@
 const apiUrl = 'http://127.0.0.1:8000/'
 
+const defaultParams = {
+  mode: 'cors',
+  redirect: 'follow',
+  referrer: 'no-referer',
+}
+
 export function fetchCategories() {
   const url = apiUrl + 'categories'
   return fetch(url, {
     method: 'GET',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
 
@@ -14,9 +18,7 @@ export function fetchLatestItems() {
   const url = apiUrl + 'items?limit=8'
   return fetch(url, {
     method: 'GET',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
 
@@ -27,9 +29,7 @@ export function fetchItem(id) {
       Authorization: window.localStorage.authToken,
     },
     method: 'GET',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
 
@@ -37,9 +37,7 @@ export function fetchCategoryItems(category_id) {
   const url = apiUrl + `categories/${category_id}/items`
   return fetch(url, {
     method: 'GET',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
 
@@ -52,10 +50,10 @@ export function addItem(data) {
       Authorization: window.localStorage.authToken,
     },
     method: 'POST',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
-  }).then(resp => resp.json())
+    ...defaultParams,
+  }).then(resp => {
+    return resp.json()
+  })
 }
 
 export function editItem(data) {
@@ -67,9 +65,7 @@ export function editItem(data) {
       Authorization: window.localStorage.authToken,
     },
     method: 'PUT',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
 
@@ -80,9 +76,7 @@ export function deleteItem(itemId) {
       Authorization: window.localStorage.authToken,
     },
     method: 'DELETE',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
 
@@ -95,8 +89,6 @@ export function addCategory(data) {
       Authorization: window.localStorage.authToken,
     },
     method: 'POST',
-    mode: 'cors',
-    redirect: 'follow',
-    referrer: 'no-referer',
+    ...defaultParams,
   }).then(resp => resp.json())
 }
